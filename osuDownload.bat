16fcf5d96493e6a7b41bd08ab586890329bbd26b
KnowScratcher <yianlee2008@gmail.com>
2024-09-14 17:54:07 +0800

    fix some issue

--- osuDownload.bat
+++ osuDownload.bat
@@ -1,7 +1,7 @@
 @echo off
 set /p "video=URL: "
-yt-dlp --embed-thumbnail --add-metadata --write-thumbnail --convert-thumbnails png --merge-output-format mp4 -o "osuvideo/%%(title)s/video.%%(ext)s" -o "thumbnail:osuvideo/%%(title)s/video.%%(ext)s" %video%
+yt-dlp --embed-thumbnail --add-metadata --write-thumbnail --convert-thumbnails png --merge-output-format mp4 -f "bv" -o "osuvideo/%%(title)s/video.%%(ext)s" -o "thumbnail:osuvideo/%%(title)s/video.%%(ext)s" %video%
 
-yt-dlp --embed-thumbnail --add-metadata  --extract-audio --audio-format mp3 -o "osuvideo/%%(title)s/audio.%%(ext)s" %video%
+yt-dlp --embed-thumbnail --add-metadata  --extract-audio --audio-format mp3 -f "ba" -o "osuvideo/%%(title)s/audio.%%(ext)s" %video%
 
-ffmpeg -i video.mp4 -vf "scale=1280:720" -an okvideo.mp4
+pause
